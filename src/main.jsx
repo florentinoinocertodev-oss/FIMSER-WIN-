4a6756e6fc0013fa5a4ed13544b22708713f9a94
Florentino Serrão <florentinoinocertodev@gmail.com>
2026-07-12 00:11:18 +0100

    Teste conexão Firestore Firebase

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,30 @@
-import { jsxDEV } from "react/jsx-dev-runtime";
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./ui/global.css";
+
+import { db } from "./firebase.js";
+import { collection, getDocs } from "firebase/firestore";
+
+async function testarFirebase() {
+  try {
+    const snapshot = await getDocs(collection(db, "users"));
+
+    console.log("Firebase conectado com sucesso!");
+
+    snapshot.forEach((doc) => {
+      console.log(doc.id, doc.data());
+    });
+
+  } catch (error) {
+    console.error("Erro Firebase:", error);
+  }
+}
+
+testarFirebase();
+
 createRoot(document.getElementById("root")).render(
-  /* @__PURE__ */ jsxDEV(React.StrictMode, { children: /* @__PURE__ */ jsxDEV(App, {}, void 0, false, {
-    fileName: "<stdin>",
-    lineNumber: 8,
-    columnNumber: 5
-  }) }, void 0, false, {
-    fileName: "<stdin>",
-    lineNumber: 7,
-    columnNumber: 3
-  })
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
